d9e48c894d477110dc1bd27d7d5342e55895bab0
sameckmeier <eckmeier41@gmail.com>
2024-04-06 19:42:12 -0400

    fixes syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     
     validates :email, :username, uniqueness: true
-    validates :email, :username, full_name, presence: true
+    validates :email, :username, :full_name, presence: true
 end
