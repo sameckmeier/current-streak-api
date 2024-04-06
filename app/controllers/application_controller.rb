e2059c796ae34ec3e429566bbb7aa14dd8429e1e
sameckmeier <eckmeier41@gmail.com>
2024-04-06 19:22:47 -0400

    adds auth helper methods to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,36 @@
 class ApplicationController < ActionController::API
+    before_action :authorized
+
+    def encode_token(payload)
+        JWT.encode(payload, ENV["JWT_SECRET_KEY"]) 
+    end
+
+    def decoded_token
+        header = request.headers["Authorization"]
+        if header
+            token = header.split(" ")[1]
+            begin
+                JWT.decode(token, ENV["JWT_SECRET_KEY"], true, algorithm: "HS256")
+            rescue JWT::DecodeError
+                nil
+            end
+        end
+    end
+
+    def current_user 
+        if decoded_token
+            if @user
+                return @user
+            end
+
+            user_id = decoded_token[0]["user_id"]
+            @user = User.find_by(id: user_id)
+        end
+    end
+
+    def authorized
+        unless !!current_user
+            render json: { message: "Please log in" }, status: :unauthorized
+        end
+    end
 end
