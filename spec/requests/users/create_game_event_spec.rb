a06250e9d1d72b3af5dc42bc9cc629d69bfa11b0
sameckmeier <eckmeier41@gmail.com>
2024-04-07 14:13:15 -0400

    adds users create game event request spec

---
+++ spec/requests/users/create_game_event_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "User Requests", type: :request do
+    describe "POST /api/user/game_events" do
+        let!(:user) { User.create(full_name: "Test User", email: "test@test.com", username: "test_user", password: "test123") }
+        let!(:token) { JWT.encode({ user_id: user.id }, "test123") }
+
+        it "returns created game_event" do
+            game = Game.create!(name: "Math Game", category: "Math", url: "https://www.mathgame.com")
+
+            post "/api/user/game_events", headers: { "Authorization": "Bearer #{token}" }, params: { game_event: { game_id: game.id }}
+
+            json = JSON.parse(response.body)
+
+            expect(json["game_event"]).to be_present
+            expect(json["game_event"]["game_id"]).to eq(game.id)
+            expect(response.status).to eq(201)
+        end
+    end
+end
