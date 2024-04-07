34388b8252684132cef61d6cfb747d3219b10cac
sameckmeier <eckmeier41@gmail.com>
2024-04-07 14:12:56 -0400

    adds users create request spec

---
+++ spec/requests/users/create_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe "User Requests", type: :request do
+    describe "POST /api/user" do
+        it "returns created user" do
+            post "/api/user", params: { full_name: "Test User", email: "test@test.com", username: "test_user", password: "test123" }
+
+            json = JSON.parse(response.body)
+
+            expect(json["user"]).to be_present
+            expect(json["user"]["email"]).to eq("test@test.com")
+            expect(response.status).to eq(201)
+        end
+    end
+end
