38d8c2664dc1afe5bf0f301bfaed827e6429dbec
sameckmeier <eckmeier41@gmail.com>
2024-04-09 17:07:57 -0400

    changes current_streak_api to game_stats_api

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "current_streak_api_production"
+  # config.active_job.queue_name_prefix = "game_stats_api_production"
 
   config.action_mailer.perform_caching = false
 
